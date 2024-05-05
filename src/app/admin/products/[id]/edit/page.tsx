ee88146c24c3d9a08a09c786121d01984eadf3b6
Tetiana <t.kobyliakova@gmail.com>
2024-05-05 17:29:26 +0300

    Add admin pages

---
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,12 @@
+import db from "@/db/db";
+import { PageHeader } from "../../../_components/PageHeader";
+import { ProductForm } from "../../_components/ProductForm";
+export default async function EditProductPage({params: {id}}:{params: {id:string}}) {
+    const product = await db.product.findUnique({where: {id}})
+    return (
+        <>
+        <PageHeader>Edit product</PageHeader>
+        <ProductForm product={product}/>
+        </>
+    )
+} 
