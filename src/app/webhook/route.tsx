5bed256bf6428b9317a3b63aeb68fbeaf607d0d6
tetianako-prog <t.kobyliakova@gmail.com>
2024-05-11 21:39:15 +0300

    Add purchase receipt email

---
+++ src/app/webhook/route.tsx
@@ -0,0 +1,52 @@
+import db from "@/db/db"
+import { NextRequest, NextResponse } from "next/server"
+import { Resend } from "resend"
+import Stripe from "stripe"
+import PurchaseReceiptEmail from "@/email/PurchaseReceipt"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+const resend = new Resend(process.env.RESEND_EMAIL_KEY as string)
+
+export async function POST(req: NextRequest) {
+    const event = await stripe.webhooks.constructEvent(
+        await req.text(),
+        req.headers.get("stripe-signature") as string,
+        process.env.STRIPE_WEBHOOK_SECRET as string
+    )
+    if(event.type === "charge.succeeded"){
+        const charge = event.data.object
+        const productId = charge.metadata.productId
+        const email = charge.billing_details.email
+        const pricePaidInCents = charge.amount
+
+        const product = await db.product.findUnique({where: {id: productId}})
+        if(product == null || email == null) return new NextResponse("Bad Request", { status: 400})
+        const userFields = {
+            email,
+            orders: { create: {productId, pricePaidInCents }}
+        }
+        const {orders: [order]} = await db.user.upsert({ 
+            where: {email},
+            create: userFields,
+            update: userFields,
+            select: {orders: {orderBy: {createdAt: "desc"}, take: 1}}
+        });
+        const downloadVerification = await db.downloadVerification.create({
+          data: {
+            productId,
+            expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+          },
+        });
+        await resend.emails.send({
+            from: process.env.SENDER_EMAIL as string,
+            to: email,
+            subject: "Order confirmation",
+            react: <PurchaseReceiptEmail 
+            product={product}
+            order={order}
+            downloadVerificationId={downloadVerification.id}/>
+        })
+        
+    } 
+    return new NextResponse()  
+} 
